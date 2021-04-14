38394469b2aa54384f33b1654b639ec88fa5d56d
nateos <sodjanathan@gmail.com>
2021-04-14 12:54:10 +0000

    Creating Page Imports Setting up Router Adding Persistent Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {Home, Products, SingleProduct, About, AuthWrapper, PrivateRoute, CartPage, Checkout, ErrorPage} from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />}>
+          <SingleProduct />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cartpage'>
+          <CartPage />
+        </Route>
+        <Route exact path='/checkout'>
+          <Checkout />
+        </Route>
+        <Route exact path='*'>
+          <ErrorPage />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import About from './AboutPage'
+import AuthWrapper from './AuthWrapper'
+import CartPage from './CartPage'
+import Checkout from './CheckoutPage'
+import ErrorPage from './ErrorPage'
+import PrivateRoute from './PrivateRoute'
+
+export { Home, Products, SingleProduct, About, AuthWrapper, PrivateRoute, CartPage, Checkout, ErrorPage }
