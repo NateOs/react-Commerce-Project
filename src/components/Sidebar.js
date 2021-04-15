29b2c823542366b1e9ab839b4a2f5701a9c4342f
nateos <sodjanathan@gmail.com>
2021-04-15 10:45:48 +0000

    toggling sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,15 +9,14 @@ import CartButtons from './CartButtons'
 import { useUserContext } from '../context/user_context'
 
 const Sidebar = () => {
-  const { openSidebar, closeSidebar } = useProductsContext()
+  const { closeSidebar, isSidebarOpen } = useProductsContext()
   
-  const isOpen = false
   return <SidebarContainer>
-    <aside className={`${isOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'}`}>
       <div className="sidebar-header">
         <img src={logo} alt="comfy sloth" className="logo" />
         <button className='close-btn' type='button'>
-          <FaTimes />
+          <FaTimes onClick={closeSidebar}/>
         </button>
       </div>
       <ul className="links">
