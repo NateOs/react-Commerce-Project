85c2352b37a42962676fbbaa0dd25a1a06aff6a1
nateos <sodjanathan@gmail.com>
2021-04-15 12:08:13 +0000

    Writing up about component Adding pageHero component on both about and checkout pages

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,7 +4,19 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+    <PageHero title='about'/>
+    <Wrapper className='page section section-center'>
+      <img src={aboutImg} alt="nice desk" />
+      <article>
+        <div className="title">
+          <h2>our story</h2>
+          <div className='underline'></div>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident aut fugit ea repudiandae illo quae quod, corrupti totam nisi voluptatibus, iste, temporibus suscipit itaque. Excepturi labore quibusdam molestias veritatis. Veniam?</p>
+      </article>
+    </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`
