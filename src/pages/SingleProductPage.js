98ef73d8eae9bee44fd341db149fe1d5948d0139
nateos <sodjanathan@gmail.com>
2021-04-16 13:08:58 +0000

    Making requests Setting Up Error and Loading States

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
 import { useProductsContext } from '../context/products_context'
-import { single_product_url as url } from '../utils/constants'
+import {  single_product_url as url } from '../utils/constants'
 import { formatPrice } from '../utils/helpers'
 import {
   Loading,
@@ -15,7 +15,24 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+  
+  const {id} = useParams() //* getting product id from router
+  const { single_products_loading:loading, single_products_error:error, fetchSingleProduct, single_product:product } = useProductsContext()
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`)
+  }, [])
+
+  if (loading) {
+     return <Loading />
+  }
+
+  if (error) {
+    return <Error />
+  }
+  return <Wrapper>
+    single product
+  </Wrapper>
 }
 
 const Wrapper = styled.main`
