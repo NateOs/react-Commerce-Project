27f7ee3c4a4bc52d5595b9aa08b480b31eed18df
nateos <sodjanathan@gmail.com>
2021-04-21 10:47:34 +0000

    Setting up color choice buttons Setting up onClick fxns Passing props to cart buttons

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -8,17 +8,46 @@ import AmountButtons from './AmountButtons'
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product
   const [mainColor, setMainColor] = useState(colors[0])
+  const [amount, setAmount] = useState(1)
+
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) {
+        tempAmount = stock
+      }
+      return tempAmount
+    })
+  }
+
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < stock) {
+        tempAmount = 1
+      }
+      return tempAmount
+    })
+  }
 
   return <Wrapper>
     <div className="colors">
       <span> colors : </span>
       <div>
         {colors.map((color, index) => {
-          return <button key={index} style={{ background: color}} className= {`${mainColor === color ? 'active color-btn' : 'color-btn' }`} onClick={()=>setMainColor(color)}> {mainColor === color ? <FaCheck/> : null } </button>
+          return <button key={index} style={{ background: color}} 
+            className= {`${mainColor === color ? 'active color-btn' : 'color-btn' }`} 
+            onClick={()=>setMainColor(color)}> {mainColor === color ? <FaCheck/> : null } 
+          </button>
         })}
       </div>
     </div>
-    <div className='btn-container'></div>
+    <div className='btn-container'>
+      <AmountButtons increase={increase} decrease= {decrease} amount={amount}/>
+      <Link to='/cart' className='btn'>
+        add to cart
+      </Link>
+    </div>
   </Wrapper>
 }
 
