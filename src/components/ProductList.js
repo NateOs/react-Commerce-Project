99c322f72d800754720a4f40e6cad6cc76a98982
nateos <sodjanathan@gmail.com>
2021-04-22 13:16:01 +0000

    Writing the conditions for either ListView or Grid

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react'
-import { useFilterContext } from '../context/filter_context'
-import GridView from './GridView'
-import ListView from './ListView'
+import React, { useEffect } from 'react';
+import { useFilterContext } from '../context/filter_context';
+import GridView from './GridView';
+import ListView from './ListView';
 
 const ProductList = () => {
-  const { filter_products:products } = useFilterContext();
-  
-  return <GridView products={products}>product list</GridView>
-}
+	const { filter_products: products, grid_view } = useFilterContext();
+	console.log(products);
 
-export default ProductList
+	if (products.length < 1) {
+		return <h5 style={{ textTransform: 'none' }}>Sorry, no products matched your search...</h5>;
+	}
+	if (grid_view === false) {
+		return <ListView products={products} />;
+	}
+	return <GridView products={products}>stuff</GridView>;
+};
+
+export default ProductList;
