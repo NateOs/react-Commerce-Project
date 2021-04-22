e96d7092ddca30cba9cafa15f3adbe2205c71cef
nateos <sodjanathan@gmail.com>
2021-04-22 19:37:39 +0000

    Adding fxn to the Grid and List buttons

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,7 @@ import { useProductsContext } from './products_context';
 const initialState = {
 	filter_products: [],
 	all_products: [],
-	grid_view: false,
+	grid_view: true,
 };
 
 const FilterContext = React.createContext();
@@ -23,12 +23,24 @@ const FilterContext = React.createContext();
 export const FilterProvider = ({ children }) => {
 	const { products } = useProductsContext(); //calling the hook to access all products from the products context
 	const [state, dispatch] = useReducer(reducer, initialState);
-	console.log(products);
+
 	useEffect(() => {
 		dispatch({ type: LOAD_PRODUCTS, payload: products });
 	}, [products]);
 
-	return <FilterContext.Provider value={{ ...state }}>{children}</FilterContext.Provider>;
+	const setGridView = () => {
+		dispatch({ type: SET_GRIDVIEW });
+	};
+
+	const setListView = () => {
+		dispatch({ type: SET_LISTVIEW });
+	};
+
+	return (
+		<FilterContext.Provider value={{ ...state, setGridView, setListView }}>
+			{children}
+		</FilterContext.Provider>
+	);
 };
 // make sure use
 export const useFilterContext = () => {
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -1,21 +1,30 @@
 import {
-  LOAD_PRODUCTS,
-  SET_LISTVIEW,
-  SET_GRIDVIEW,
-  UPDATE_SORT,
-  SORT_PRODUCTS,
-  UPDATE_FILTERS,
-  FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
+	LOAD_PRODUCTS,
+	SET_LISTVIEW,
+	SET_GRIDVIEW,
+	UPDATE_SORT,
+	SORT_PRODUCTS,
+	UPDATE_FILTERS,
+	FILTER_PRODUCTS,
+	CLEAR_FILTERS,
+} from '../actions';
 
 const filter_reducer = (state, action) => {
+	if (action.type === LOAD_PRODUCTS) {
+		// this time we dont directly assign the state values to prevent a gotcha, we spread instead
+		return { ...state, all_products: [...action.payload], filter_products: [...action.payload] };
+	}
 
-  if (action.type === LOAD_PRODUCTS) {
-    // this time we dont directly assign the state values to prevent a gotcha, we spread instead
-    return {...state,  all_products : [...action.payload], filter_products: [...action.payload]} 
-  }
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+	if (action.type === SET_GRIDVIEW) {
+		// this time we dont directly assign the state values to prevent a gotcha, we spread instead
+		return { ...state, grid_view: true };
+	}
 
-export default filter_reducer
+	if (action.type === SET_LISTVIEW) {
+		// this time we dont directly assign the state values to prevent a gotcha, we spread instead
+		return { ...state, grid_view: false };
+	}
+	throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default filter_reducer;
