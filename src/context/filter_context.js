7a7557f47b6dc18102238c133c0c113d1f7d89ac
nateos <sodjanathan@gmail.com>
2021-04-23 09:13:31 +0000

    Minor change, removed code

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -41,7 +41,6 @@ export const FilterProvider = ({ children }) => {
 	const updateSort = (e) => {
 		//* for demo
 		//* const name = e.target.name
-		const name = e.target.name;
 		const value = e.target.value;
 		dispatch({ type: UPDATE_SORT, payload: value });
 	};
