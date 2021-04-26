45ec61ccf2d36389b9012704c23732ea239068ed
nateos <sodjanathan@gmail.com>
2021-04-26 20:22:08 +0000

    if btn name = categ then value = btn text content This gets the button class to active or inactive state Also enable the spec btn action dispatched

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -65,7 +65,7 @@ const filter_reducer = (state, action) => {
 	}
 
 	if (action.type === FILTER_PRODUCTS) {
-		console.log('filtering');
+		// console.log('filtering');
 		return { ...state, };
 	}
 
