ee825f90b4418b3f7c5ecf18e8bfb2b1875234ab
nateos <sodjanathan@gmail.com>
2021-04-27 23:53:24 +0000

    Filtering the products by searchbox Entry

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -65,8 +65,18 @@ const filter_reducer = (state, action) => {
 	}
 
 	if (action.type === FILTER_PRODUCTS) {
-		// console.log('filtering');
-		return { ...state };
+		const { all_products } = state;
+		const { text, company, category, color, min_price, max_price, price, shipping } = state.filters;
+
+		let tempProducts = [...all_products];
+
+		if (text) {
+			tempProducts = tempProducts.filter((product) => {
+				return product.name.toLowerCase().startsWith(text)
+			})
+		}
+
+		return { ...state, filter_products: tempProducts };
 	}
 	if (action.type === CLEAR_FILTERS) {
 		return {
