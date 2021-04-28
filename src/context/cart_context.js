39ca76642e5e63c843fded6e648b4753ec01917a
nateos <sodjanathan@gmail.com>
2021-04-28 13:31:52 +0000

    Passing addToCart funcion to cartContext Prettified AddtoCart Component Added addToCart as onClick

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useContext, useReducer } from 'react';
-import reducer from '../reducers/cart_reducer';
+import React, { useEffect, useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
 import {
 	ADD_TO_CART,
 	REMOVE_CART_ITEM,
 	TOGGLE_CART_ITEM_AMOUNT,
 	CLEAR_CART,
 	COUNT_CART_TOTALS,
-} from '../actions';
-import { initialAuthState } from '@auth0/auth0-react/dist/auth-state';
+} from "../actions";
+// import { initialAuthState } from '@auth0/auth0-react/dist/auth-state';
 
 const initialState = {
 	cart: [],
@@ -20,7 +20,16 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
-	return <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>;
+
+	//* addToCart function
+	const addToCart = (id, color, amount, product) => {
+		dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+	};
+	return (
+		<CartContext.Provider value={{ ...state, addToCart }}>
+			{children}
+		</CartContext.Provider>
+	);
 };
 // make sure use
 export const useCartContext = () => {
