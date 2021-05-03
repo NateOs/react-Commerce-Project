641d109625bc5b5085bc84b5f872d2be8230ceb4
nateos <sodjanathan@gmail.com>
2021-05-03 20:14:59 +0000

    feature: inc or dec item.amount on cart

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,7 +7,6 @@ import {
 } from "../actions";
 
 const cart_reducer = (state, action) => {
-
 	if (action.type === ADD_TO_CART) {
 		const { id, color, amount, product } = action.payload;
 		const tempItem = state.cart.find((item) => item.id === id + color);
@@ -46,11 +45,36 @@ const cart_reducer = (state, action) => {
 		);
 		return { ...state, cart: tempCartItem };
 	}
-	
+
 	if (action.type === CLEAR_CART) {
 		return { ...state, cart: [] };
 	}
 
+	if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+		const { id, value } = action.payload;
+		//* find cart item by id and increase or decrease amount by 1
+		const tempCart = state.cart.map((item) => {
+			if (id === item.id) {
+				if (value === "inc") {
+					let newAmount = item.amount + 1;
+					if (newAmount > item.max) {
+						newAmount = item.max;
+					}
+					return { ...item, amount: newAmount };
+				}
+				if (value === "dec") {
+					let newAmount = item.amount - 1;
+					if (newAmount < 1) {
+						newAmount = 1;
+					}
+					return { ...item, amount: newAmount };
+				}
+			} else {
+				return item;
+			}
+		});
+		return { ...state, cart: tempCart };
+	}
 	throw new Error(`No Matching "${action.type}" - action type`);
 };
 
