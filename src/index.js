9186f9767aa41bbecc12e26189d7133108df17e4
nateos <sodjanathan@gmail.com>
2021-05-04 21:17:21 +0000

    feature: setting up auth0 setting up the provider

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { ProductsProvider } from './context/products_context';
-import { FilterProvider } from './context/filter_context';
-import { CartProvider } from './context/cart_context';
-import { UserProvider } from './context/user_context';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ProductsProvider } from "./context/products_context";
+import { FilterProvider } from "./context/filter_context";
+import { CartProvider } from "./context/cart_context";
+import { UserProvider } from "./context/user_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-	<ProductsProvider>
-		{/* Wrapping FilterProvider into Products provider allows filterProvider access to products context, where we get all products*/}
-		<FilterProvider>
-			<CartProvider>
-				<App />
-			</CartProvider>
-		</FilterProvider>
-	</ProductsProvider>,
-	document.getElementById('root'),
+	<Auth0Provider
+		domain="dev-r5-tir1y.us.auth0.com"
+		clientId="9jCcYBkNUESKNbi3klioZ4pCMTsjnN7d"
+		redirectUri={window.location.origin}
+		cacheLocation={localStorage}>
+		<ProductsProvider>
+			{/* Wrapping FilterProvider into Products provider allows filterProvider access to products context, where we get all products*/}
+			<FilterProvider>
+				<CartProvider>
+					<App />
+				</CartProvider>
+			</FilterProvider>
+		</ProductsProvider>
+	</Auth0Provider>,
+	document.getElementById("root"),
 );
