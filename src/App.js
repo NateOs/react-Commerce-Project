cf84b63aa0c928e0fff5d8c6c5f902369e1f3bf0
nateos <sodjanathan@gmail.com>
2021-05-10 21:53:52 +0000

    bugfix: adding wrapper component fixing /checkout bug with authWrapper Wrapping whole app fixes url nav to checkout issue

--- src/App.js
+++ src/App.js
@@ -1,39 +1,51 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
-import {Home, Products, SingleProduct, About, AuthWrapper, PrivateRoute, CartPage, Checkout, ErrorPage} from './pages'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+	Home,
+	Products,
+	SingleProduct,
+	About,
+	AuthWrapper,
+	PrivateRoute,
+	CartPage,
+	Checkout,
+	ErrorPage,
+} from "./pages";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Sidebar />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/products'>
-          <Products />
-        </Route>
-        <Route exact path='/products/:id' children={<SingleProduct />}>
-          <SingleProduct />
-        </Route>
-        <Route exact path='/about'>
-          <About />
-        </Route>
-        <Route exact path='/cartpage'>
-          <CartPage />
-        </Route>
-        <PrivateRoute exact path='/checkout'>
-          <Checkout />
-        </PrivateRoute>
-        <Route exact path='*'>
-          <ErrorPage />
-        </Route>
-      </Switch>
-      <Footer />
-    </Router>
-  )
+	return (
+		<AuthWrapper>
+			<Router>
+				<Navbar />
+				<Sidebar />
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/products">
+						<Products />
+					</Route>
+					<Route exact path="/products/:id" children={<SingleProduct />}>
+						<SingleProduct />
+					</Route>
+					<Route exact path="/about">
+						<About />
+					</Route>
+					<Route exact path="/cartpage">
+						<CartPage />
+					</Route>
+					<PrivateRoute exact path="/checkout">
+						<Checkout />
+					</PrivateRoute>
+					<Route exact path="*">
+						<ErrorPage />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
+		</AuthWrapper>
+	);
 }
 
-export default App
+export default App;
