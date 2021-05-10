1342cde0ca3fe503076abc2a0ee05689f4418d21
nateos <sodjanathan@gmail.com>
2021-05-10 21:52:29 +0000

    bugfix: checking and getting user directly Getting from auth0 directly is faster

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,16 +1,17 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 // will remove later
 import { useUserContext } from "../context/user_context";
 
 const PrivateRoute = ({ children, ...rest }) => {
-	const { myUser } = useUserContext();
+	const { user } = useAuth0();
 
 	return (
 		<Route
 			{...rest}
 			render={() => {
-				return myUser ? children : <Redirect to="/"></Redirect>;
+				return user ? children : <Redirect to="/"></Redirect>;
 			}}></Route>
 	);
 };
